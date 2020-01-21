c6b6d4896a7418e78346a1b33610264aff9da399
santhosh <santhoshkumar.ch74@gmail.com>
2020-01-21 16:08:58 +0530

    commit from IST

--- src/test/java/mavenTest/SeleniumTest.java
+++ src/test/java/mavenTest/SeleniumTest.java
@@ -7,6 +7,8 @@ public class SeleniumTest {
 	@Test
 	public void mavenTestTutorial() {
 		System.out.println("Testing Maven");
+		System.out.println("Testing Maven2");
+		System.out.println("Testing Maven3");
 	}
 
 }
