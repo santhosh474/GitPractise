02b0706f784675f025b80c1a4e1830673d723076
santhosh <santhoshkumar.ch74@gmail.com>
2020-01-21 15:16:57 +0530

    first commit

---
+++ src/test/java/mavenTest/SeleniumTest.java
@@ -0,0 +1,12 @@
+package mavenTest;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void mavenTestTutorial() {
+		System.out.println("Testing Maven");
+	}
+
+}
---
+++ src/test/java/mavenTest/TestNGTest.java
@@ -0,0 +1,12 @@
+package mavenTest;
+
+import org.testng.annotations.Test;
+
+public class TestNGTest {
+	
+	@Test
+	public void sampleTestNGTest() {
+		System.out.println("Testing Sample TestNG test");
+	}
+
+}
